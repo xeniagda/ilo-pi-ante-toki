cb255efb84e75e3dc07e1eb132eeeafdf2af90ab
loovjo <jonathan.loov@gmail.com>
2020-04-26 18:15:34 +0200

    continue working on tokens.rs

--- load-data/tokens.rs
+++ load-data/tokens.rs
@@ -2,6 +2,8 @@ use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use std::fmt::Debug;
 
+use std::io::Write;
+
 #[derive(Debug, Copy, Clone)]
 enum Gram<I> {
     Orig(I),
@@ -9,8 +11,10 @@ enum Gram<I> {
 }
 
 // Retuns the tokenized text, along with a list of decompositions
-fn encode_into_ngrams<I: Debug + Copy + Eq + Hash, F: Fn(&I) -> bool>(inp: Vec<I>, n_pairs: usize, can_pair: F) -> (Vec<usize>, Vec<Gram<I>>) {
+fn encode_into_ngrams<I: Debug + Copy + Eq + Hash, F: Fn(&I) -> bool>(inp: Vec<I>, rel_lim: f64, can_pair: F) -> (Vec<usize>, Vec<Gram<I>>) {
     // Convert the text into orig tokens
+
+    let inp_len = inp.len() as f64;
 
     let mut tokens: Vec<usize> = Vec::new();
     let mut grams = Vec::new();
@@ -35,16 +39,19 @@ fn encode_into_ngrams<I: Debug + Copy + Eq + Hash, F: Fn(&I) -> bool>(inp: Vec<I
     // println!("Tokens: {:?}", tokens);
     // println!("Grams: {:?}", grams);
 
-    for i in 0..n_pairs {
-        eprintln!("{}", i);
+    loop {
         let pair_freq = get_pair_freq(&tokens, &skips);
-        let (&commonest_pair, freq) = if let Some(x) = pair_freq.iter().max_by_key(|(_, &count)| count) {
+        let (&commonest_pair, &freq) = if let Some(x) = pair_freq.iter().max_by_key(|(_, &count)| count) {
             x
         } else {
             eprintln!("Ran out of pairs: {:?} / {:?} - {:?}", tokens, grams, skips);
             return (tokens, grams);
         };
-        // println!("Commonest: {:?}, freq {}", commonest_pair, freq);
+        let rel_freq = freq as f64 / inp_len as f64;
+        println!("Commonest: {:?}, freq {}, {}%", commonest_pair, freq, rel_freq);
+        if rel_freq < rel_lim {
+            break;
+        }
 
         let new_gram_idx = grams.len();
         grams.push(Gram::Composition(commonest_pair.0, commonest_pair.1));
@@ -123,6 +130,49 @@ fn decompose_sequence<I: Eq + Hash + Copy>(mut tokens: Vec<usize>, grams: &[Gram
         .collect()
 }
 
+// Assumes ngrams are in topolocical order
+// Format:
+// Gram::Orig(ch): chl = utf-8 len of ch
+// offset: 0x0 0x1 0x2 0x3 0x4 0x5 0x6 0x7 0x8
+//  value: chl ---  ch...  --- 0x0 0x0 0x0 0x0
+//
+// Gram::Composition(a, b) encodes:
+// offset: 0x0 0x1 0x2 0x3 0x4 0x5 0x6 0x7 0x8
+//  value: 0x0 -------a------- -------b-------
+
+fn encode_grams<F: Write>(out: &mut F, grams: Vec<Gram<char>>) -> std::io::Result<()> {
+    // Assert topoorder
+    for (i, gram) in grams.iter().enumerate() {
+        match gram {
+            &Gram::Composition(a, b) => debug_assert!(a < i && b < i),
+            _ => {}
+        }
+    }
+
+    for gram in grams {
+        match gram {
+            Gram::Orig(ch) => {
+                let chl = ch.len_utf8();
+                out.write(&[chl as u8])?; // Safe to covert since ch.len_utf8() <= 4
+                let mut ch_buf = [0u8; 8];
+                ch.encode_utf8(&mut ch_buf);
+                out.write(&ch_buf)?;
+            }
+            Gram::Composition(a, b) => {
+                out.write(&[0])?;
+
+                let a_bytes = (a as u32).to_le_bytes();
+                let b_bytes = (b as u32).to_le_bytes();
+
+                out.write(&a_bytes)?;
+                out.write(&b_bytes)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     use std::io::Read;
 
@@ -130,12 +180,10 @@ fn main() -> std::io::Result<()> {
     let mut buf = Vec::new();
     f.read_to_end(&mut buf)?;
 
-    let text = String::from_utf8_lossy(&buf);
-
-    let inp = text.chars().collect::<Vec<_>>();
+    let inp = String::from_utf8_lossy(&buf).chars().collect();
 
     println!("Encoding");
-    let (stream, grams) = encode_into_ngrams(inp, 1000, |&ch| ch != ' ');
+    let (stream, grams) = encode_into_ngrams(inp, 0.001, |&ch| true);
 
     // println!("{:?}", decompose_sequence(stream, &grams));
     for i in 0..grams.len() {
@@ -145,8 +193,11 @@ fn main() -> std::io::Result<()> {
             print!("* ");
         }
         let expanded_gram = decompose_sequence(vec![i], &grams);
-        println!("{} -> {:?}", i, expanded_gram.into_iter().collect::<String>());
+        println!("{} -> {:?}", i, expanded_gram.iter().collect::<String>());
     }
+
+    let mut out = std::fs::File::create("/tmp/gramcode.bin")?;
+    encode_grams(&mut out, grams)?;
 
     Ok(())
 }
